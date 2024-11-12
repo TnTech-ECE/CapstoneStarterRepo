0ba9a60b2558043207c4e35c2a29d435bc89644d
Adam Morrow <143036960+awmorrow42@users.noreply.github.com>
2024-11-12 15:47:40 -0600

    Update CapstoneProject.ino
    updates Code

--- Code/Velocity/CapstoneProject.ino
+++ Code/Velocity/CapstoneProject.ino
@@ -11,7 +11,26 @@ void setup()
    // Initialize serial for output.
    SerialPort.begin(115200);
    SerialPort.println("Starting...");
+   while(!Serial){}
+   delay(1000);
 
+   //Print Welcome
+   Serial.printf("Welcome to the networker!!\n");
+
+   // Will print '.' to the serial until it connects to the access point
+   Serial.printf("Connecting to %s ", SSID);
+   WiFi.hostname(HOST_NAME);
+   WiFi.begin(SSID, PASSWORD);
+
+   while (WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.print(".");
+   }
+
+  Serial.println(" connected");
+
+  Udp.begin(LOCAL_UDP_PORT);
+  Serial.printf("Now listening at IP %s, UDP port %d, remote IP: %s\n", WiFi.localIP().toString().c_str(), LOCAL_UDP_PORT, Udp.remoteIP().toString().c_str());
    // Initialize I2C bus.
    DEV_I2C.begin();
 
@@ -34,6 +53,10 @@ void setup()
 
 void loop()
 {
+   if(start == True){
+    servo();
+   }
+   check_for_signal();
    VL53LX_MultiRangingData_t MultiRangingData;
    VL53LX_MultiRangingData_t *pMultiRangingData = &MultiRangingData;
    uint8_t NewDataReady = 0;
@@ -72,7 +95,70 @@ void loop()
       }
       digitalWrite(LedPin, LOW);
    }
+}
 
+//Checks if serial avalible and then reads chars into char array
+void check_for_signal() {
+  if (Serial.available()) {
+      String message = ""; 
+      // Build the message
+      while (Serial.available()) {
+        char c = Serial.read();
+        message += c;
+        delayMicroseconds(1000);
+      }
+      // Display the message on the Serial monitor
+      Serial.print("Received: ");
+      Serial.println(message);
+      // Send the packet over udp
+      forwardData((char*)message.c_str());
+      i = atoi(message);
+    }
+}
+
+// Check if packet is recieved. Returns true if packet is recieved else: false
+bool recievedPacket(){
+  int packetSize = Udp.parsePacket();
+  //if packetSize is null then no packet has been recieved
+  if (packetSize){
+    // receive incoming UDP packets
+    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    remoteIp = Udp.remoteIP();
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    // Display contents to serial
+    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+    // send back a reply by repeating the packet back
+    //sendPacket(incomingPacket);
+    return true;
+  }
+  return false;
+}
+
+// Do not use directly, use forwardData. Sends Packet with payload that is packetData
+void sendPacket(char packetData[]){
+    Udp.beginPacket(REMOTE_IP, REMOTE_UDP_PORT);
+    Udp.write((const char*)packetData, PAYLOAD_LEN+1);
+    Udp.endPacket();
+    //Serial.printf("Sent to %s, port %d\n", Udp.remoteIP().toString().c_str(), Udp.remotePort());
+}
+
+// Sends packet and adds the ID
+inline void forwardData(char data[]){
+  char payload[PAYLOAD_LEN+1];
+  payload[0] = ID;
+  for(int i = 0; i < PAYLOAD_LEN; i++){
+    payload[i+1] = data[i];
+  }
+  /*
+  for(int i = 0; i < PAYLOAD_LEN+1; i++){
+    Serial.print(payload[i]);
+  }
+  */
+  sendPacket(payload);
 }
 
 void measure()
@@ -90,13 +176,14 @@ void servo(){
     }
   }
   else{
-    while(servoPos1 <= lineDegreex[j]) {
+    while(servoPos1 <= lineDegreex[i]) {
       servoPos1++;
       servo1.write(servoPos1);
       Serial.println(servoPos1);
       delay(5);
     }
   }
+
   if(lineDegreey[j] <= servoPos2){
     while(servoPos2 >= lineDegreey[j]) {
       servoPos2--;
