122e1b3ebe51cf972e0c8db12c75c8d5bd20ffb1
Adam Morrow <143036960+awmorrow42@users.noreply.github.com>
2024-11-12 15:48:12 -0600

    Update capstoneproject.h
    Updated File

--- Code/Velocity/capstoneproject.h
+++ Code/Velocity/capstoneproject.h
@@ -9,6 +9,9 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ESP32Servo.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
 /**********Status**********/
 #define CAPSTONEPROJECT_H
 #define  VL53LX_RANGESTATUS_RANGE_VALID       0
@@ -50,18 +53,45 @@
 #endif
 #define LedPin LED_BUILTIN
 #define interruptPin A2
+#define LOCAL_UDP_PORT 1337     //Any port as long as the firewall is fine with it
+#define REMOTE_UDP_PORT 1337    //Probably should be the same as local
+#define PAYLOAD_LEN 1
+#define ID '0'
+
+const IPAddress REMOTE_IP(10, 104, 130, 255);
+
+// WiFi Credentials
+const char SSID[] = "EagleNet";
+const char PASSWORD[] = "cY4DoKPo";
+// Arbitrary name
+const char HOST_NAME[] = "NetworkStation1";
+IPAddress remoteIp;
+
+WiFiUDP Udp;
+char incomingPacket[256];  // buffer for incoming packets
+
+void sendPacket(char packetData[]);
+void check_for_signal()
 
 VL53LX sensor_vl53lx_sat(&DEV_I2C, A1);
 volatile int interruptCount=0;
 
+bool start = True;
+
 Servo servo1;
 Servo servo2;
-long time
-static const int servoPin1 = 13;
-static const int servoPin2 = 14;
-int i,j = 0;
-int lineDegreex[15];
-int lineDegreey[15];
-int distancemm = 0;
-int servoPos1 = 30;
+int servoPos1 = 25;
 int servoPos2 = 90;
+
+static bool b_receive = false;
+const uint8_t u8_length = 128;
+char ac_input[u8_length] = {0};
+uint8_t u8_receivedPackets;
+
+long time
+static const int servoPin1 = A4;
+static const int servoPin2 = A3;
+int i,j = 0;
+int lineDegreex[15] = {155,146,137,128,119,110,101,92,83,74,65,56,47,38,29};
+int lineDegreey[4] = {25,35,45,55};
+int distancemm = 0;
