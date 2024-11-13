64b4eb9309de0a46385e1b808a7f89650f8024af
Adam Morrow <143036960+awmorrow42@users.noreply.github.com>
2024-11-13 12:55:35 -0600

    Update capstoneproject.h
    Made Updates to code in servo, loop, added serial input to change servo position

--- Code/Velocity/capstoneproject.h
+++ Code/Velocity/capstoneproject.h
@@ -9,7 +9,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ESP32Servo.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <WiFiUdp.h>
 #include <SPI.h>
 /**********Status**********/
@@ -48,50 +48,54 @@
 #define  VL53LX_RANGESTATUS_NONE        255
 #define DEV_I2C Wire
 #define SerialPort Serial
+//WiFi
+#define LOCAL_UDP_PORT 1337     //Any port as long as the firewall is fine with it
+#define REMOTE_UDP_PORT 1337    //Probably should be the same as local
+#define PAYLOAD_LEN 6
+#define ID 0x042
+//Led and Pin definitions
+#define LedPin LED_BUILTIN
+#define interruptPin A2
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
-#define LedPin LED_BUILTIN
-#define interruptPin A2
-#define LOCAL_UDP_PORT 1337     //Any port as long as the firewall is fine with it
-#define REMOTE_UDP_PORT 1337    //Probably should be the same as local
-#define PAYLOAD_LEN 1
-#define ID '0'
-
-const IPAddress REMOTE_IP(10, 104, 130, 255);
 
 // WiFi Credentials
+const IPAddress REMOTE_IP(10, 104, 133, 219);
 const char SSID[] = "EagleNet";
 const char PASSWORD[] = "cY4DoKPo";
-// Arbitrary name
 const char HOST_NAME[] = "NetworkStation1";
 IPAddress remoteIp;
-
 WiFiUDP Udp;
 char incomingPacket[256];  // buffer for incoming packets
 
-void sendPacket(char packetData[]);
-void check_for_signal()
-
+//VL53LX Data
 VL53LX sensor_vl53lx_sat(&DEV_I2C, A1);
+uint32_t time_ms;
 volatile int interruptCount=0;
 
-bool start = True;
+//Servo
+Servo servo1;//X direction servo
+Servo servo2;//Y direction servo
+int servoPos1 = 0;//Default position
+int servoPos2 = 0;//Default position
+static const int servoPin1 = A3;//ShortWire
+static const int servoPin2 = A4;
+uint8_t distancemm = 0;
+uint8_t i,j = 0;
+uint8_t lineDegreex[16] = {90,155,146,137,128,119,110,101,92,83,74,65,56,47,38,29};
+//90,29,38,47,56,65,74,83,92,101,110,119,128,137,146,155
+uint8_t lineDegreey[3] = {0,45,130};
 
-Servo servo1;
-Servo servo2;
-int servoPos1 = 25;
-int servoPos2 = 90;
+//Serial Input Data
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+boolean newData = false;
 
-static bool b_receive = false;
-const uint8_t u8_length = 128;
-char ac_input[u8_length] = {0};
-uint8_t u8_receivedPackets;
-
-long time
-static const int servoPin1 = A4;
-static const int servoPin2 = A3;
-int i,j = 0;
-int lineDegreex[15] = {155,146,137,128,119,110,101,92,83,74,65,56,47,38,29};
-int lineDegreey[4] = {25,35,45,55};
-int distancemm = 0;
+//Functions
+void serial_input();
+void output_input();
+void sendPacket(char packetData[]);
+void check_for_signal();
+void servo();
+void measure();
